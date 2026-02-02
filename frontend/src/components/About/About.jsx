c812a6b172d663ae74a37e8826ad5a8d100d6778
AISWARYA <76909@TCS.Local>
2026-02-02 13:05:25 +0530

    push some changes

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from 'react-parallax-tilt'
-import profileImage from '../../assets/image.png';
+import profile from '../../assets/pro.png';
 
 const About = () => {
   return (
@@ -53,11 +53,13 @@ const About = () => {
           transitionSpeed={1000}
           gyroscope={true}
           >
-            <img
-              src={profileImage}
-              alt="Profile"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
-            />
+          <img
+            src={profile}
+            alt="Profile"
+            className="w-full h-full rounded-full object-cover object-[50%_25%] drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
+          />
+
+
           </Tilt>
         </div>
 
